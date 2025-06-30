89462cb7a1201f4e7153904cfd6bbaaf9f10cc21
AmatrackIos <ama@e-steps.support>
2025-06-30 18:48:19 +0530

    Initial SwiftUI project setup for StringCalculatorApp

---
+++ StringCalculatorApp/StringCalculatorApp/StringCalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StringCalculatorAppApp.swift
+//  StringCalculatorApp
+//
+//  Created by Mriganka De on 30/06/25.
+//
+
+import SwiftUI
+
+@main
+struct StringCalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
