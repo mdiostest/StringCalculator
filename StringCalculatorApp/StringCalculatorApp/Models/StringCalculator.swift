a0b43374fddd3f4e02f578f799d8575afd8d04e9
AmatrackIos <ama@e-steps.support>
2025-06-30 19:14:48 +0530

    Make first test pass: add("") returns 0 and Restructure files into MVVM folders: Models, Views

---
+++ StringCalculatorApp/StringCalculatorApp/Models/StringCalculator.swift
@@ -0,0 +1,14 @@
+//
+//  StringCalculator.swift
+//  StringCalculatorApp
+//
+//  Created by Mriganka De on 30/06/25.
+//
+
+import Foundation
+
+class StringCalculator {
+    func add(_ numbers: String) throws -> Int {
+        return 0
+    }
+}
